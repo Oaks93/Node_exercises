27e1d934157183efa55a099df6518f6dff5d7522
Carlos Gómez Linares <16gcarlos.93@gmail.com>
2023-08-24 12:35:10 +0200

    complete database

--- Exercise_10/server.js
+++ Exercise_10/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     console.log(`Received request: ${req.method} ${req.url}`);
     next()
 })
@@ -27,5 +27,10 @@ let planetsDatabase = [
     { id: 1, name: 'Mercury' },
     { id: 2, name: 'Venus' },
     { id: 3, name: 'Earth' },
-    
+    { id: 4, name: 'Mars' },
+    { id: 5, name: 'Jupiter' },
+    { id: 6, name: 'Saturn' },
+    { id: 7, name: 'Uranus' },
+    { id: 8, name: 'Neptune' },
+    { id: 9, name: 'Pluto' }
 ];
